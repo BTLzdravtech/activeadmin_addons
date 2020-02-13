4cbdcc243653fefa3e363c04527ee05275412b9b
Tomáš Hruška <hruska@btl.cz>
2020-02-13 15:25:15 +0100

    Fix getting model name

--- lib/activeadmin_addons/support/input_helpers/input_methods.rb
+++ lib/activeadmin_addons/support/input_helpers/input_methods.rb
@@ -23,8 +23,7 @@ module ActiveAdminAddons
     end
 
     def method_model
-      object_class.reflect_on_association(association_name).try(:klass) ||
-        association_name.classify.constantize
+      object_class.reflect_on_association(association_name).try(:klass) || object_class
     end
 
     def tableize_method
